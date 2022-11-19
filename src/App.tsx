bc1c90283ac109064f31ca9c3d7bb3a3879c01fe
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2022-11-19 17:51:54 -0300

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Button } from './components'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secundary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
