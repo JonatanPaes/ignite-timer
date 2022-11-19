1c4a6e1015a743d3c6894d4e47f9485ab6b9e49d
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2022-11-19 13:50:41 -0300

    Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "styled-components";
+
 import { Button } from "./components";
+import { defaultTheme } from "./components/styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secundary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   );
 }
--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -16,9 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100%;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
 
-  ${(props) =>
+  /* ${(props) =>
     css`
       background-color: ${buttonVariants[props.variant]};
-    `}
+    `} */
 `;
